dd27c53eeae6b33f6c210c484bee741844d79002
Peter Melisko <pmelisko@gratex.com>
2024-10-14 10:14:42 +0200

    [TEST] added tests, run all schemas and check if transform will fail

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 /* jshint esversion: 6 */
 const assert = require("assert");
+const { globSync } = require("glob");
 const fs = require("fs");
 const endOfLine = require('os').EOL;
 
@@ -22,4 +23,48 @@ describe("schema2doc", function() {
 	it("to raw format", function() {
 		assert.equal(toRaw(simpleSchema).trim().replace(/\r\n/g,endOfLine), rawSimpleOut.trim(), "Raw format doesnt match with expected format");
 	});
+
+	it ("generic test", function() {
+		
+	})
+});
+
+describe("Load test files - check if not failing", function() {
+
+    function unrollDataAsyncTask(done) {
+        // fs, request whatever async here, callback or promise
+        //glob("../../../15**/**/index.js", {
+        const files = globSync("./input/*.json", {
+            cwd: __dirname,
+			dotRelative: true
+        });
+
+		done(null, files);
+    }
+    before(function(done) {
+        this.timeout(2000);
+        var addTest = this.test.parent.addTest.bind(this.test.parent);
+        // call asynct task and generate tests
+        unrollDataAsyncTask(function(err, datas) {
+            datas.map(_it_test).forEach(addTest);
+            done();
+        });
+    });
+    // make at least on emethod, so before gets called
+    it("Unroll Data shall exist", function(done) {
+        unrollDataAsyncTask(function(err, datas) {
+            assert(datas.length > 0);
+            done();
+        });
+    });
+    // then this will become many it() registered in before();
+    function _it_test(mid) {
+        return it(`schema: ${mid}`, function() {
+            var schema = require(mid);
+            
+			assert.ok(toRaw(schema), 'raw transform should not fail');
+			assert.ok(toCsv(schema), 'cvs transform should not fail')
+			assert.ok(toHtml(schema), 'html transform should not fail')
+        });
+    }
 });
